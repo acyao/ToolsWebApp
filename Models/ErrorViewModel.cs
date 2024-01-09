0885ffcd393eba04456e80c4e0062b452efabd7d
acyao <ac_yao94@hotmail.com>
2024-01-09 20:54:59 +0800

    added image to base64

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToolsWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
